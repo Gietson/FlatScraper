b06394d57be9e882c0cab8cdb32a3705635ed2dd
Gietson <gietson1@gmail.com>
2017-08-31 10:46:00 +0200

    fix tests

---
+++ src/FlatScraper.Infrastructure/Repositories/AdRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlatScraper.Core.Domain;
+using FlatScraper.Core.Repositories;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace FlatScraper.Infrastructure.Repositories
+{
+    public class AdRepository : IAdRepository, IMongoRepository
+    {
+        private readonly IMongoDatabase _database;
+
+        public AdRepository(IMongoDatabase database)
+        {
+            _database = database;
+        }
+        public async Task<Ad> GetAsync(Guid id)
+            => await Ad.AsQueryable().FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task<IEnumerable<Ad>> GetAllAsync()
+            => await Ad.AsQueryable().ToListAsync();
+
+        public async Task AddAsync(Ad page)
+            => await Ad.InsertOneAsync(page);
+
+        public async Task UpdateAsync(Ad scan)
+            => await Ad.ReplaceOneAsync(x => x.Id == scan.Id, scan);
+
+        public async Task RemoveAsync(Guid id)
+            => await Ad.DeleteOneAsync(x => x.Id == id);
+
+        private IMongoCollection<Ad> Ad => _database.GetCollection<Ad>("Ad");
+
+    }
+}
---
+++ src/FlatScraper.Infrastructure/Repositories/ScanPageRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FlatScraper.Core.Domain;
+using FlatScraper.Core.Repositories;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace FlatScraper.Infrastructure.Repositories
+{
+    public class ScanPageRepository : IScanPageRepository, IMongoRepository
+    {
+        private readonly IMongoDatabase _database;
+
+        public ScanPageRepository(IMongoDatabase database)
+        {
+            _database = database;
+        }
+        public async Task<ScanPage> GetAsync(Guid id)
+            => await ScanPage.AsQueryable().FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task<IEnumerable<ScanPage>> GetAllAsync()
+            => await ScanPage.AsQueryable().ToListAsync();
+
+        public async Task AddAsync(ScanPage page)
+            => await ScanPage.InsertOneAsync(page);
+
+        public async Task UpdateAsync(ScanPage scan)
+            => await ScanPage.ReplaceOneAsync(x => x.Id == scan.Id, scan);
+
+        public async Task RemoveAsync(ScanPage page)
+            => await ScanPage.DeleteOneAsync(x=>x.Id == page.Id);
+
+        private IMongoCollection<ScanPage> ScanPage => _database.GetCollection<ScanPage>("ScanPage");
+
+    }
+}
