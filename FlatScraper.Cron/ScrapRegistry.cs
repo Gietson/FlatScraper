70d496ca74fc76ed65ea6fb01ab3aa8b58d6ca36
Gietson <gietson1@gmail.com>
2017-09-11 13:41:38 +0200

    log

--- FlatScraper.Cron/ScrapRegistry.cs
+++ FlatScraper.Cron/ScrapRegistry.cs
@@ -1,13 +1,17 @@
 ﻿using FlatScraper.Core.Repositories;
 using FlatScraper.Infrastructure.Services;
 using FluentScheduler;
+using Serilog;
 
 namespace FlatScraper.Cron
 {
     public class ScrapRegistry : Registry
     {
+        private static readonly ILogger Logger = Log.Logger;
+
         public ScrapRegistry(IScanPageService scanPageService, IAdRepository adRepository)
         {
+            Log.Debug("Start Scraper Task!");
             Schedule(async () => await new ScraperService(scanPageService, adRepository).ScrapAsync()).ToRunNow().AndEvery(2).Minutes();
         }
     }
